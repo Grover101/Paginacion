4811a14731c9be777f122f33fb597e697b103015
Grover101 <limachigrover09@gmail.com>
2020-01-30 11:03:28 -0400

    Trayendo los articulos de 5 en 5 de la base de datos

--- index.php
+++ index.php
@@ -9,5 +9,16 @@ try {
 	die();
 }
 
+$pagina=isset($_GET['pagina'])? (int)$_GET['pagina'] : 1;
+$postPorPagina=5;
+
+$inicio=($pagina>1)?($pagina*$postPorPagina-$postPorPagina):0;
+
+$articulos=$conexion->prepare("SELET SQL_CALC_FOUND_ROWS * FORM articulos LIMT $inicio, $postPorPagina");
+
+$articulos->execute();
+$articulos=$articulos->fetchAll();
+
+print_r($articulos);
 
 require_once 'index.view.php';
