631d4e3cc5f394e2f81393db96885be33365375c
Grover101 <limachigrover09@gmail.com>
2020-01-30 10:41:52 -0400

    Agregamos el index.view.php solo es hmtl

---
+++ index.view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Paginacion</title>
+	<link rel="stylesheet" href="estilos.css">
+	<link href="https://fonts.googleapis.com/css?family=Gochi+Hand&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="contenedor">
+		<h1>Articulos</h1>
+		<section class="articulos">
+			<ul>
+				<li>Lorem ipsum dolor sit amet.</li>
+				<li>Lorem ipsum dolor sit amet.</li>
+				<li>Lorem ipsum dolor sit amet.</li>
+				<li>Lorem ipsum dolor sit amet.</li>
+			</ul>
+		</section>
+
+		<section class="paginacion">
+			<ul>
+				<li class="disabled"><a href="#">&laquo;</a></li>
+				<li class="active"><a href="#">1</a></li>
+				<li><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a href="#">4</a></li>
+				<li><a href="#">&raquo;</a></li>
+			</ul>
+		</section>
+	</div>
+</body>
+</html>
