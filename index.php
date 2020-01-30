3610f09dae1ff277fca6aec3d75420f19d5e3e18
Grover101 <limachigrover09@gmail.com>
2020-01-30 11:02:00 -0400

    Conexion a la base de datos

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
+try {
+	
+	$conexion=new PDO('mysql:host=localhost; dbname=Paginacion','root','');
+
+} catch (PDOException $e) {
+	echo "ERROR: ".$e->getMessage();
+	die();
+}
+
+
 require_once 'index.view.php';
