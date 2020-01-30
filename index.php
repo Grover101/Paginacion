bf30f3cd2f75fd096d423d9d585a43a99533b4dd
Grover101 <limachigrover09@gmail.com>
2020-01-30 11:28:20 -0400

    Corrigiendo los errores

--- index.php
+++ index.php
@@ -9,16 +9,28 @@ try {
 	die();
 }
 
-$pagina=isset($_GET['pagina'])? (int)$_GET['pagina'] : 1;
-$postPorPagina=5;
+$pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+$postPorPagina = 5;
 
-$inicio=($pagina>1)?($pagina*$postPorPagina-$postPorPagina):0;
-
-$articulos=$conexion->prepare("SELET SQL_CALC_FOUND_ROWS * FORM articulos LIMT $inicio, $postPorPagina");
+$inicio = ($pagina > 1) ? ($pagina * $postPorPagina - $postPorPagina) : 0 ;
+$articulos = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio, $postPorPagina");
 
 $articulos->execute();
-$articulos=$articulos->fetchAll();
+$articulos = $articulos->fetchAll();
 
-print_r($articulos);
+// print_r($articulos);
+
+if(!$articulos){
+	header('Location: http://localhost/Paginación/');
+}
+
+
+$totalArticulos=$conexion->query('SELECT FOUND_ROWS() AS total');
+$totalArticulos=$totalArticulos->fetch()['total'];
+
+$numeroPaginas=ceil($totalArticulos/$postPorPagina);
+
+echo $numeroPaginas;
+
 
 require_once 'index.view.php';
